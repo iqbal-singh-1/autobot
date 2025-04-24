6d16c6b89bea24057245dea12d159f786c5e98e8
iqbal-singh-1 <iqbalsingh2434@gmail.com>
2025-04-25 00:29:37 +0530

    fixed file structure

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/autobot/autobot', // 👈 this line is important for GitHub Pages
+  base: '/autobot/', // 👈 this line is important for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
