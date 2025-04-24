442c7ca4319c056ba8b932a309a8bec58cb8f106
iqbal-singh-1 <iqbalsingh2434@gmail.com>
2025-04-25 00:04:43 +0530

    fixing github pages setup

--- autobot/vite.config.ts
+++ autobot/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/autobot-chatbot/', // 👈 this line is important for GitHub Pages
+  base: '/autobot/autobot', // 👈 this line is important for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
